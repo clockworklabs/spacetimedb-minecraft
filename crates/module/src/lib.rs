ea2625a77f9fc659110de799660ea693162ba52d
John Detter <no-reply@boppygames.gg>
2024-01-19 11:38:26 -0600

    Day 4 - Terrain generation is finally working in SpacetimeDB

--- crates/module/src/lib.rs
+++ crates/module/src/lib.rs
@@ -15,25 +15,29 @@ pub struct StdbChunk {
 /// Server world seed is currently hardcoded.
 pub const SEED: i64 = 9999;
 
-#[spacetimedb(reducer)]
-pub fn generate_chunk() {
-    let generator = OverworldGenerator::new(SEED);
-    let mut state = <OverworldGenerator as ChunkGenerator>::State::default();
-
+#[spacetimedb(init)]
+pub fn init() {
     log::info!("Starting Generation");
 
     for x in -3..4 {
         for z in -3..4 {
-            let mut chunk = Chunk::new_no_arc();
-            generator.gen_terrain(x, z, &mut chunk, &mut state);
-            if let Err(_) = StdbChunk::insert(StdbChunk {
-                chunk_id: 0, x, z, chunk,
-            }) {
-                log::error!("Failed to insert unique chunk");
-            };
-            log::info!("Chunk Generated: {}, {}", x, z);
+            generate_chunk(x, z);
         }
     }
 
     log::info!("Generation complete");
+}
+
+#[spacetimedb(reducer)]
+pub fn generate_chunk(x: i32, z: i32) {
+    let generator = OverworldGenerator::new(SEED);
+    let mut state = <OverworldGenerator as ChunkGenerator>::State::default();
+    let mut chunk = Chunk::new_no_arc();
+    generator.gen_terrain(x, z, &mut chunk, &mut state);
+    if let Err(_) = StdbChunk::insert(StdbChunk {
+        chunk_id: 0, x, z, chunk,
+    }) {
+        log::error!("Failed to insert unique chunk");
+    };
+    log::info!("Chunk Generated: {}, {}", x, z);
 }
